ef92b32305fc2cd4106e238a19159a3d09775e2b
Krishav-ajay <krishav-ajay.prakash.student.liljeholmen@engelska.se>
2024-07-24 16:47:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+    console.log(document);
+    let R = prompt("Write the passcode for this website");
+    if (R === "!S!r.i!h!A:r=s/h(I)n=i:(") {
+        alert('Hello');
+        console.log('Hello Sriharshini{!S!r.i!h!A:r=s/h(I)n=i:("}');
+    } else {
+        function links() {
+           //document.body.style.display = "none";
+        }
+        links();
+        if (console === Error) {
+           // document.body.style.display = "none";
+        }
+    }
+}
